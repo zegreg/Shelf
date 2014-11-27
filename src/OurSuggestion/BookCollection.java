6f30991f5058f972647cb573b0db5876c27fab22
Lucas <lucas.andrade234@gmail.com>
2014-11-27 10:28:08 +0000

    ele chora por todo o lado.

--- src/OurSuggestion/BookCollection.java
+++
@@ -1,7 +0,0 @@
-package OurSuggestion;
-
-import java.util.List;
-
-public class BookCollection  extends Collection{
-	List<Book> books;
-}
--- src/OurSuggestion/DVDCollection.java
+++
@@ -1,7 +0,0 @@
-package OurSuggestion;
-
-import java.util.List;
-
-public class DVDCollection extends Collection {
-	List<DVD> dvd;
-}
