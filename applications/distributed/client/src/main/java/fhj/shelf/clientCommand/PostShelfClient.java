954b092a6d76cd660f8b414767246a6846501ab9
jose <jose@oliveira.com>
2015-02-16 10:27:11 +0000

    uri

--- applications/distributed/client/src/main/java/fhj/shelf/clientCommand/PostShelfClient.java
+++ applications/distributed/client/src/main/java/fhj/shelf/clientCommand/PostShelfClient.java
@@ -57,7 +57,7 @@ public class PostShelfClient extends  BaseClientCommand implements UIPostCommand
 
 		StringBuilder builder = new StringBuilder();
 
-		builder.append(super.getRequestURL()).append("?").append(path);
+		builder.append(super.getRequestURL()).append(path).append("?");
 
 		 
 		
